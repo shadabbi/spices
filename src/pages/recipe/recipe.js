fc26bc4f3f14fa083c1572edc20b1276fed255c9
shadabbi <shadab.ali7503@gmail.com>
2020-11-01 10:07:28 +0530

    initial commit

---
+++ src/pages/recipe/recipe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './recipe.module.scss';
+
+const recipe = (props) => {
+
+    return(
+        <div>recipe</div>
+    )
+}
+
+export default recipe;
